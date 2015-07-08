b016c495436a6548e484547d6c927a248bb3e9d5
Thomas Ruiz <contact@thomasruiz.eu>
2015-07-09 00:26:19 +0100

    move Track into namespace audio

--- KentDAW/Source/core/AudioTrack.h
+++ KentDAW/Source/core/AudioTrack.h
@@ -8,45 +8,19 @@
   ==============================================================================
 */
 
-#ifndef TRACK_H_INCLUDED
-#define TRACK_H_INCLUDED
+#ifndef AUDIO_TRACK_H_INCLUDED
+#define AUDIO_TRACK_H_INCLUDED
 
 #include "../JuceLibraryCode/JuceHeader.h"
 
-class Track : public AudioSource
-{
-public:
-    
-     // types of track
-    enum TrackType { None = 0, Audio, Midi };
-     // track controls
-    enum ControlType { Record, Mute, Solo };
-     // constructor
-    Track();
-     // destructor
-    ~Track();
-     // clear the track
-     void clear();
-     // open track
-     void open();
-     // close track
-     void close();
-     // set track type
-     void setTrackType(TrackType type);
-     // get track type
-     TrackType getTrackType() const;
-     // set track name
-     void setTrackName(String name);
-     // get track name
-     String getTrackName();
-     
-    
-    void prepareToPlay(int expectedSamplePerBlock, double sampleRate);
-    void releaseResources();
-    void getNextAudioBlock(const AudioSourceChannelInfo &bufferToFill);
-     
-private:
-     OwnedArray<AudioSampleBuffer>& clips;
-};
+namespace audio {
+	class Track : public AudioSource {
+	public:
+		void prepareToPlay(int expectedSamplePerBlock, double sampleRate);
+		void releaseResources();
+		void getNextAudioBlock(const AudioSourceChannelInfo& bufferToFill);
+	};
+}
 
-#endif  // TRACK_H_INCLUDED
+#endif // AUDIO_TRACK_H_INCLUDED
+
--- KentDAW/Source/ui/ArrangeWindow.cpp
+++ KentDAW/Source/ui/ArrangeWindow.cpp
@@ -10,7 +10,6 @@
 
 #include "../JuceLibraryCode/JuceHeader.h"
 #include "ArrangeWindow.h"
-#include "../utils/BufferDisplay.h"
 
 //==============================================================================
 ArrangeWindow::ArrangeWindow()
