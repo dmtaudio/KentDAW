9615756d860c0d02010e2c53601c2a4b08a9c960
Matthew Rumsey <mmr22@kent.ac.uk>
2015-07-09 22:23:45 +0100

    Committing to carry on with dan

---
+++ KentDAW/Source/core/Region.h
@@ -0,0 +1,27 @@
+/*
+  ==============================================================================
+
+    Region.h
+    Created: 9 Jul 2015 9:10:36pm
+    Author:  Matt
+
+  ==============================================================================
+*/
+
+#ifndef REGION_H_INCLUDED
+#define REGION_H_INCLUDED
+
+struct Region
+{
+	/*
+	The UNIQUE ID of a region
+	*/
+	int regionID;
+	int startPostion;
+	int endPosition;
+	int regionBeginPosition;
+};
+
+
+
+#endif  // REGION_H_INCLUDED
