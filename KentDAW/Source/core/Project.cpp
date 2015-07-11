048d2b0aeb6d1b3cacb38d0198712871afb17af4
dtl <dtl@eduDCDF.kent.ac.uk>
2015-07-11 09:55:40 +0100

    Added a project class for file management.

---
+++ KentDAW/Source/core/Project.cpp
@@ -0,0 +1,16 @@
+/*
+  ==============================================================================
+
+    Project.cpp
+    Created: 11 Jul 2015 9:26:48am
+    Author:  dtl
+
+  ==============================================================================
+*/
+
+#include "Project.h"
+
+void Project::createBasicProject(const String& projName)
+{
+    XmlElement projectFile("PROJECT SETTINGS");
+}
---
+++ KentDAW/Source/core/Project.h
@@ -0,0 +1,29 @@
+/*
+  ==============================================================================
+
+    Project.h
+    Created: 11 Jul 2015 9:26:48am
+    Author:  dtl
+
+  ==============================================================================
+*/
+
+#ifndef PROJECT_H_INCLUDED
+#define PROJECT_H_INCLUDED
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+class Project
+{
+    
+public:
+    Project();
+    ~Project();
+    void createBasicProject(const String& projName);
+    
+private:
+};
+
+
+
+#endif  // PROJECT_H_INCLUDED
