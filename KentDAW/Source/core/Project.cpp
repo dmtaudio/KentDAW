89c230c4ed55be22848480d0deb582eeed487ed4
dtl <dtl@eduD9EC.kent.ac.uk>
2015-07-11 12:03:09 +0100

    Added a simple way of adding paths to project file, this will undoubtably need to be changed to accommodate how paths are actually stored. Currently just using a JUCE String Array

--- KentDAW/Source/core/Project.cpp
+++ KentDAW/Source/core/Project.cpp
@@ -10,13 +10,22 @@
 
 #include "Project.h"
 
-void Project::createBasicProject(const String& projName)
+void Project::createBasicProject(const String& projName, const Array<String> projFiles)
 {
     XmlElement project("PROJECT");
-    
     XmlElement* projectSettings = new XmlElement("Settings");
     
     projectSettings->setAttribute("name", projName);
+    
+    XmlElement files("FILES");
+    XmlElement* projectFiles = new XmlElement("Paths");
+    
+    for(auto file : projFiles)
+    {
+        int i = 1;
+        projectFiles->setAttribute((String)i, file);
+        i++;
+    }
     
     project.addChildElement(projectSettings);
     String xmlDoc = project.createDocument(String::empty);
--- KentDAW/Source/core/Project.h
+++ KentDAW/Source/core/Project.h
@@ -19,7 +19,7 @@ class Project
 public:
     Project();
     ~Project();
-    void createBasicProject(const String& projName);
+    void createBasicProject(const String& projName, const Array<String>);
     
 private:
 };
