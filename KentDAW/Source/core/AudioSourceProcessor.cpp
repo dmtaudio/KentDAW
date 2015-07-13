fc03954bab19f8d086fa4211a2d340d8c0fe30b2
dtl <dtl@eduDDE8.kent.ac.uk>
2015-07-13 14:30:48 +0100

    AudioSourceProcessor Wrapper completed.

--- KentDAW/Source/core/AudioSourceProcessor.cpp
+++ KentDAW/Source/core/AudioSourceProcessor.cpp
@@ -9,3 +9,49 @@
 */
 
 #include "AudioSourceProcessor.h"
+
+AudioSourceProcessor::AudioSourceProcessor(AudioSource* const input, const bool deleteSource_)
+: source(input),
+  deleteSource(deleteSource_)
+{}
+
+AudioSourceProcessor::~AudioSourceProcessor()
+{
+    if(deleteSource)
+        delete source;
+}
+
+const String AudioSourceProcessor::getName() const
+{
+    return String("AudioSourceWrapper");
+}
+
+const String AudioSourceProcessor::getInputChannelName(const int channelIndex) const
+{
+    return String(channelIndex + 1);
+}
+
+const String AudioSourceProcessor::getOutputChannelName(const int channelIndex) const
+{
+    return String(channelIndex + 1);
+}
+
+void AudioSourceProcessor::prepareToPlay(double sampleRate, int samplesPerBlock)
+{
+    source->prepareToPlay(samplesPerBlock, sampleRate);
+}
+
+void AudioSourceProcessor::releaseResources()
+{
+    source->releaseResources();
+}
+
+void AudioSourceProcessor::processBlock(AudioSampleBuffer &buffer, MidiBuffer &midiMessages)
+{
+    AudioSourceChannelInfo channelInfo;
+    channelInfo.buffer = &buffer;
+    channelInfo.startSample = 0;
+    channelInfo.numSamples = buffer.getNumSamples();
+    source->getNextAudioBlock(channelInfo);
+    
+}
--- KentDAW/Source/core/AudioSourceProcessor.h
+++ KentDAW/Source/core/AudioSourceProcessor.h
@@ -24,14 +24,31 @@ public:
     void releaseResources();
     
     const String getName() const;
-    const String getInputChannelName() const;
-    const String getOutputChannelName() const;
+    const String getInputChannelName(const int channelIndex) const;
+    const String getOutputChannelName(const int channelIndex) const;
     
     bool isInputChannelStereoPair(int index) const { return false; }
     bool isOutputChannelStereoPair(int index) const { return false; }
     
-    bool acceptsMidi() const = 0;
-    bool producesMidi() const = 0;
+    bool acceptsMidi() const { return false; }
+    bool producesMidi() const { return false; }
+    
+    int getNumParameters() { return 0; }
+    const String getParameterName(int paramaterIndex) { return String::empty; }
+    float getParameter(int parameterIndex) { return 0.0f; }
+    const String getParameterText(int parameterIndex) { return String::empty; }
+    void setParameter(int parameterIndex, float newValue);
+    
+    int getNumPrograms() { return 0; }
+    int getCurrentProgram() { return 0; }
+    void setCurrentProgram(int index);
+    const String getProgramName(int index) { return String::empty; }
+    void changeProgramName(int index,const String& name);
+    
+    void getStateInformation(MemoryBlock& destData);
+    void setStateInformation(const void* data, int sizeBytes);
+    
+    AudioProcessorEditor* createEditor() { return 0; }
     
 private:
     AudioSource* source;
