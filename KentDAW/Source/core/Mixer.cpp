2089a2711089dc7c46d10e6f71a83922a2c19004
Matthew Rumsey <mmr22@kent.ac.uk>
2015-07-15 13:57:52 +0100

    Beginnings of the mixer class
    The stuff in the constructer may be best off set within the audio engine
    at a later date.
    +Add functionality that sets programs for gain(fader), panning, mute,
    and solo
    + Create functionality to apply gain(fader), panning, mute and solo to
    each track as its put into the mixer
    + Add master output functionality
    +Link all of the above to nodes within the graph

--- KentDAW/Source/core/Mixer.cpp
+++ KentDAW/Source/core/Mixer.cpp
@@ -9,3 +9,53 @@
 */
 
 #include "Mixer.h"
+
+Mixer::Mixer() {
+	inputNode = new AudioProcessorGraph::AudioGraphIOProcessor(AudioProcessorGraph::AudioGraphIOProcessor::audioInputNode);
+	outputNode = new AudioProcessorGraph::AudioGraphIOProcessor(AudioProcessorGraph::AudioGraphIOProcessor::audioOutputNode);
+	setPlayConfigDetails(0, 2, 44100, 512);
+	addNode(inputNode);
+	addNode(outputNode);
+	addConnection(1, 1, 2, 1);
+	prepareToPlay(44100, 512);
+
+	trackNumber = 1;
+}
+
+Mixer::~Mixer() {
+}
+
+AudioSourceProcessor Mixer::createProcessorFromSource(AudioTrack* source)
+{
+	AudioSourceProcessor asProcessor(source, false);
+	//sources.push_back(asProcessor);
+	sourceProcessors.set(trackNumber, asProcessor);
+	trackNumber++;
+
+	return asProcessor;
+}
+
+void Mixer::addMuteControl()
+{
+
+}
+
+void Mixer::addPanningControl()
+{
+
+}
+
+void Mixer::addFaderControl()
+{
+
+}
+
+void Mixer::addtoGraph()
+{
+
+}
+
+void Mixer::removeFromGraph(uint32 trackID)
+{
+	removeNode(trackID);
+}
--- KentDAW/Source/core/Mixer.h
+++ KentDAW/Source/core/Mixer.h
@@ -11,8 +11,33 @@
 #ifndef MIXER_H_INCLUDED
 #define MIXER_H_INCLUDED
 
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "AudioTrack.h"
+#include "AudioSourceProcessor.h"
+
+class Mixer	: public AudioProcessorGraph
+{
+public:
+	Mixer();
+	~Mixer();
+
+	AudioSourceProcessor createProcessorFromSource(AudioTrack* source);
+	void addMuteControl();
+	void addPanningControl();
+	void addFaderControl();
+
+	void addtoGraph();
+	void removeFromGraph(uint32 trackID);
+	int trackNumber;
 
 
+private:
+	//std::list<AudioSourceProcessor> sources;
+	HashMap<uint32, AudioSourceProcessor> sourceProcessors;
+	AudioGraphIOProcessor* inputNode;
+	AudioGraphIOProcessor* outputNode;
+
+};
 
 
 #endif  // MIXER_H_INCLUDED
