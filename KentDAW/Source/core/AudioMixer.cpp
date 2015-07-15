5e8a785791120eb43f46543ac4bbe17a8835fe97
Matthew Rumsey <mmr22@kent.ac.uk>
2015-07-15 16:22:40 +0100

    Renamed Mixer to AudioMixer

--- KentDAW/Source/core/AudioMixer.cpp
+++ KentDAW/Source/core/AudioMixer.cpp
@@ -1,103 +1,61 @@
 /*
   ==============================================================================
 
-    region.cpp
-    Created: 9 Jul 2015 8:35:39pm
+    Mixer.cpp
+    Created: 11 Jul 2015 1:51:41pm
     Author:  Matt
 
   ==============================================================================
 */
 
-#include "AudioMixer.h"
+#include "Mixer.h"
 
-AudioMixer::AudioMixer()
-	: sampleBuffer(2, 0),
-	nextPlayPosition(0),
-	totalLength(0),
-	samplesPerBlockExpected(0),
-	sampleRate(44100.0),
-	bufferingEnabled(false)
-{
+Mixer::Mixer() {
+	inputNode = new AudioProcessorGraph::AudioGraphIOProcessor(AudioProcessorGraph::AudioGraphIOProcessor::audioInputNode);
+	outputNode = new AudioProcessorGraph::AudioGraphIOProcessor(AudioProcessorGraph::AudioGraphIOProcessor::audioOutputNode);
+	setPlayConfigDetails(0, 2, 44100, 512);
+	addNode(inputNode);
+	addNode(outputNode);
+	addConnection(1, 1, 2, 1);
+	prepareToPlay(44100, 512);
+
+	trackNumber = 1;
 }
 
-AudioMixer::~AudioMixer()
-{
-	removeAllRegions();
+Mixer::~Mixer() {
 }
 
-bool AudioMixer::addRegion(const int& regionID,
-	const int& startPosition,
-	const int& endPosition,
-	const int& regionBeginPosition,
-	const String& abosolutePathToFile,
-	const double& sampleRateOfAudioDevice)
+void Mixer::createProcessorFromSource(AudioTrack* source)
 {
-	int index;
-	bool foundTheRegion = findRegion(regionID, index);
-	if (foundTheRegion)
-	{
-		return false;
-	}
-
-	File audioFile(abosolutePathToFile);
-
-	AudioFormatManager audioFormatManager;
-	audioFormatManager.registerBasicFormats();
-
-	AudioFormatReader* audioFormatReader = audioFormatManager.createReaderFor(audioFile);
-
-	if (audioFormatReader == 0)
-	{
-		delete audioFormatReader;
-		return false;
-	}
-	else if (startPosition >= endPosition
-		|| startPosition < regionBeginPosition
-		|| endPosition - regionBeginPosition > audioFormatReader->lengthInSamples)
-	{
-		return false;
-	}
-	else
-	{
-		Region* audioRegionToAdd = new Region();
-		audioRegionToAdd->regionID = regionID;
-		audioRegionToAdd->startPostion = startPosition;
-		audioRegionToAdd->endPosition = endPosition;
-		audioRegionToAdd->regionBeginPosition = regionBeginPosition;
-//		PositionableAudioSource *positionableAudioSource = new PositionableAudioSource();
-//
-//		audioRegionToAdd->;
-
-		regions.add(audioRegionToAdd);
-
-		if (totalLength < endPosition)
-		{
-			totalLength = endPosition;
-		}
-
-		return true;
-	}
+	//AudioSourceProcessor asProcessor(source, false);
+	//sources.push_back(asProcessor);
+	//sourceProcessors.set(trackNumber, asProcessor);
+	//sources.set(trackNumber, *source);
+	//addNode(&asProcessor, trackNumber); //temporary untill other functions written
+	//trackNumber++;
 }
 
-bool AudioMixer::modifyRegion(const int& regionID,
-	const int& newStartPosition,
-	const int& newEndPosition,
-	const int& newRegionBeginPosition)
+void Mixer::addMuteControl()
 {
-//	int index;
-//	bool foundTheRegion = findRegion(regionID, index);
-//
-//	if (foundTheRegion)
-//	{
-//		Region* audioRegionToModify = (Region*)regions[index];
-//
-//		if (newStartPosition >= newEndPosition
-//			|| newStartPosition < newRegionBeginPosition
-//			|| newEndPosition - newRegionBeginPosition)
-//		{
-//
-//		}
-//			
-//	}
-	return false;
+
+}
+
+void Mixer::addPanningControl()
+{
+
+}
+
+void Mixer::addFaderControl()
+{
+
+}
+
+void Mixer::addtoGraph()
+{
+
+}
+
+void Mixer::removeFromGraph(uint32 trackID)
+{
+	removeNode(trackID);
 }
