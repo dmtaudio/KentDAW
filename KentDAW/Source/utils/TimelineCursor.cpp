b3b08b65abc043c696e885449bd759d0c4c761f7
dtl <dtl@eduDF26.kent.ac.uk>
2015-08-04 18:38:19 +0100

    Timeline curser, working in theory.

---
+++ KentDAW/Source/utils/TimelineCursor.cpp
@@ -0,0 +1,136 @@
+/*
+  ==============================================================================
+
+    TimelineCursor.cpp
+    Created: 4 Aug 2015 1:51:35pm
+    Author:  dtl
+
+  ==============================================================================
+*/
+
+#include "TimelineCursor.h"
+
+TimelineCursor::TimelineCursor(AudioTransportSource& source)
+: _transporSource(source), _currentSampleRate(44100.0), _zoomRatio(1.0),
+  _offsetRatio(0.0), _stopTimer(false), _showCursor(true)
+{
+    //_transporSource.addChangeListener(this);
+}
+
+TimelineCursor::~TimelineCursor()
+{
+    //_transporSource.removeChangeListener(this);
+    stopTimer();
+}
+
+void TimelineCursor::setZoomRatio(double zoomRatio)
+{
+    if(zoomRatio < 0.0 || zoomRatio > 10000.0)
+        zoomRatio = 1.0;
+    
+    _zoomRatio = zoomRatio;
+    repaint();
+}
+
+void TimelineCursor::setStartOffsetRatio(double startOffset)
+{
+    _offsetRatio = startOffset;
+    repaint();
+}
+
+void TimelineCursor::setCursorVisability(bool displayCursor)
+{
+    _showCursor = displayCursor;
+    startTimer();
+}
+
+void TimelineCursor::paint(juce::Graphics &g)
+{
+    if(_showCursor)
+        g.drawImageAt(cursor, _currentXCoords - 1, 0);
+}
+
+void TimelineCursor::resized()
+{
+    cursor = Image(Image::RGB, 3, jmax(1, getHeight()), true);
+    Graphics g(cursor);
+    g.fillAll(Colours::black);
+    g.setColour(Colours::grey);
+    g.drawVerticalLine(1, 0.0f, (float) cursor.getHeight());
+}
+
+void TimelineCursor::timerCallback()
+{
+    const int firstPixel = roundToInt(getWidth() * _offsetRatio);
+    _previousXCoords = _currentXCoords;
+    _currentXCoords = firstPixel + roundToInt(getWidth() * _oneOverTrackLength * _transporSource.getCurrentPosition() / _zoomRatio);
+    if(_currentXCoords != _previousXCoords)
+    {
+        repaint(_previousXCoords = 2, 0, 5, getHeight());
+        repaint(_currentXCoords = 2, 0, 5, getHeight());
+    }
+    if(_stopTimer)
+    {
+        _stopTimer = false;
+        stopTimer();
+    }
+}
+
+void TimelineCursor::transportStateChanged(AudioTransportSource *source)
+{
+    if(source == &_transporSource)
+        startTimer();
+}
+
+void TimelineCursor::mouseDown(const MouseEvent &e)
+{
+    if(_showCursor)
+    {
+        setMouseCursor(MouseCursor::IBeamCursor);
+        _xScale = (float) ((_trackLength / getWidth() * _zoomRatio));
+        setPlayerPosition(e.x);
+        
+        _stopTimer = false;
+        Timer::startTimer(40);
+        repaint();
+    }
+}
+
+void TimelineCursor::mouseUp(const MouseEvent &e)
+{
+    if(_showCursor)
+    {
+        setMouseCursor(MouseCursor::NormalCursor);
+        startTimer();
+    }
+}
+
+void TimelineCursor::mouseDrag(const MouseEvent &e)
+{
+    if(_showCursor)
+        setPlayerPosition(e.x);
+}
+
+void TimelineCursor::startTimer()
+{
+    if(_showCursor)
+    {
+        _stopTimer = false;
+        Timer::startTimer(40);
+    }
+    else
+    {
+        _stopTimer = true;
+    }
+}
+
+void TimelineCursor::setPlayerPosition(int mouseX)
+{
+    const int firstPixel = roundToInt(getWidth() * _offsetRatio);
+    double position = _xScale * (_mouseX - firstPixel);
+    _transporSource.setPosition(position);
+}
+
+
+
+
---
+++ KentDAW/Source/utils/TimelineCursor.h
@@ -0,0 +1,62 @@
+/*
+  ==============================================================================
+
+    TimelineCursor.h
+    Created: 4 Aug 2015 1:51:35pm
+    Author:  dtl
+
+  ==============================================================================
+*/
+
+#ifndef TIMELINECURSOR_H_INCLUDED
+#define TIMELINECURSOR_H_INCLUDED
+
+#include "../../JuceLibraryCode/JuceHeader.h"
+
+class TimelineCursor : public Component,
+                       public Timer
+                       //public ChangeListener
+{
+
+public:
+    
+    TimelineCursor(AudioTransportSource& source);
+    ~TimelineCursor();
+    
+    void setZoomRatio(double zoomRatio);
+    void setStartOffsetRatio(double startOffset);
+    void setCursorVisability(bool displayCursor);
+    
+    void transportStateChanged(AudioTransportSource* source);
+    
+    void paint(Graphics &g);
+    void resized();
+    
+    void mouseDown(const MouseEvent &e);
+    void mouseUp(const MouseEvent &e);
+    void mouseDrag(const MouseEvent &e);
+    
+    void timerCallback();
+    
+private:
+    AudioTransportSource& _transporSource;
+    double _currentSampleRate;
+    double _trackLength, _oneOverTrackLength;
+    double _zoomRatio, _offsetRatio;
+    bool _stopTimer, _showCursor;
+    
+    Image cursor;
+    
+    int _previousXCoords, _currentXCoords;
+    float _xScale;
+    double _mouseX;
+    
+    void startTimer();
+    void setPlayerPosition(int mouseX);
+    
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (TimelineCursor);
+};
+
+
+
+#endif  // TIMELINECURSOR_H_INCLUDED
